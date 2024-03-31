fb7bd67777ffe2e86c49dae81746d103d23ad99c
username <barasamica@gmail.com>
2024-03-31 19:56:07 +0300

    Created index.js file

---
+++ src/assets/images/index.js
@@ -0,0 +1,10 @@
+import logo from './WhatsApp Image 2024-03-22 at 1.34.57 PM.jpeg';
+import image from './WhatsApp Image 2024-03-26 at 20.48.56 (1).jpeg';
+import wall from './pexels-tomáš-malík-4090321.jpg';
+
+
+export {
+    logo,
+    image,
+    wall
+};
